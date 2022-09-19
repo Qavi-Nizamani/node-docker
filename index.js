8422fd143baa3b67586f8cdae1aad4e9ee489e16
Qavi-Nizamani <Qaviniz11@gmail.com>
2022-09-19 16:40:16 +0500

    first tutorial complete

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 const app = express();
+
+mongoose
+  .connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/mydb?authSource=admin`,
+  )
+  .then(() => console.log('Successfully connected'))
+  .catch((e) => console.log(e));
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi</h2>');
