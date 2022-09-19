cb5f5cd73cdd36594632aa28317167d139c5fa81
Qavi-Nizamani <Qaviniz11@gmail.com>
2022-09-19 13:19:30 +0500

    Fixed nodemon issue

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('h2>Hi there</h2>');
+  res.send('<h2>Hi 2!!!</h2>');
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('I am running: ', port);
+  console.log('I am always running: ', port);
 });
